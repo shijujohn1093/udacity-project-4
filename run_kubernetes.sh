5351adf4735559bc0c2edaa0ac5683efa1c79edb
EC2 Default User <ec2-user@ip-172-31-27-32.us-west-2.compute.internal>
2020-10-02 08:27:42 +0000

    Kubernetes changes

--- run_kubernetes.sh
+++ run_kubernetes.sh
@@ -5,14 +5,19 @@
 # Step 1:
 # This is your Docker ID/path
 # dockerpath=<>
+dockerpath="shijujohn1093/project4"
 
 # Step 2
 # Run the Docker Hub container with kubernetes
-
+kubectl run udacityproject4\
+    --generator=run-pod/v1\
+    --image=$dockerpath\
+    --port=80 --labels app=udacityproject4
 
 # Step 3:
 # List kubernetes pods
+kubectl get pods
 
 # Step 4:
 # Forward the container port to a host
-
+kubectl port-forward udacityproject4 8000:80
