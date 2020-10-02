c4a77ddc74786d3a98dff362eb10c853661228b9
Shiju John <shijujohn1093@gmail.com>
2020-10-02 07:27:00 +0000

    container changes

--- upload_docker.sh
+++ upload_docker.sh
@@ -6,10 +6,13 @@
 # Step 1:
 # Create dockerpath
 # dockerpath=<your docker ID/path>
+dockerpath="shijujohn1093/project4"
 
 # Step 2:  
 # Authenticate & tag
 echo "Docker ID and Image: $dockerpath"
+docker login
 
 # Step 3:
 # Push image to a docker repository
+docker push $dockerpath
